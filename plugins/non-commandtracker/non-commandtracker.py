94388b0ba022f46c4d593e8ee5b5799419e33e09
Felix Hedenström <fhede@kth.se>
2019-06-11 17:11:52 +0200

    Added the non-command tracker

--- plugins/non-commandtracker/non-commandtracker.py
+++ plugins/non-commandtracker/non-commandtracker.py
@@ -8,16 +8,15 @@ Created 14-04-2018
 from plugins.plugintemplate import PluginTemplate
 import libs.nlp.similar
 
-class NonCommandTracker(PluginTemplate):
+class NonCommandTrackerPlugin(PluginTemplate):
     def __init__(self, bot):
-        self._config = bot.get_config()
         super().__init__(bot, description="Keeps track of when users call for non-existing commands.")
 
         def ffilter(message):
 
             text = message.get_text().strip()
 
-            if not text[0] == self._config['global']['prefix']:
+            if not text[0] == self._bot.get_command_prefix():
                 return False
 
             if text[1:].split(" ")[0] in self._bot.get_commands().keys():
@@ -25,7 +24,7 @@ class NonCommandTracker(PluginTemplate):
 
             return True
 
-        self._bot.add_event_listener('on_message', self.respond, prefilter=ffilter)
+        self._bot.add_event_listener('on_message', self.respond, prefilter=ffilter )
 
     def respond(self, message):
 
@@ -33,16 +32,14 @@ class NonCommandTracker(PluginTemplate):
 
         text = text[1:]
         command = text.split(" ")[0]
-        
-        if __debug__:
-            self.log('Found a non-existing command: "{}."'.format(command))
+        self.log('Found a non-existing command: "{}."'.format(command))
 
-        return_message_text = 'Command "{prefix}{command}" was not found. Try using "{prefix}commands" for a list of commands.'.format(command=command, prefix=self._config['global']['prefix'])
+        returnmessagetext = 'Command "{prefix}{command}" was not found. Try using "{prefix}commands" for a list of commands.'.format(command=command, prefix=self._bot.get_command_prefix())
 
-        #if self._config.getboolean('non-commandtracker', 'suggestCommands'):
-        commands = '", "{prefix}'.join(libs.nlp.similar.possibilities(command, list(self._bot.get_commands().keys()))).format(prefix=self._config['global']['prefix'])
-        return_message_text += '\nCould you have ment: "{prefix}{commands}"?'.format(commands=commands, prefix=self._config['global']['prefix'])
+        if True: # TODO make this configurable
+            commands = '", "{prefix}'.join(libs.nlp.similar.possibilities(command, list(self._bot.get_commands().keys()))).format(prefix=self._bot.get_command_prefix())
+            returnmessagetext += '\nCould you have ment: "{prefix}{commands}"?'.format(commands=commands, prefix=self._bot.get_command_prefix())
 
-        ans = message.respond(RuntimeError(return_message_text), self._bot.get_nick())
+        ans = message.respond(RuntimeError(returnmessagetext), self._bot.get_nick())
 
         self._bot.add_message(ans)
